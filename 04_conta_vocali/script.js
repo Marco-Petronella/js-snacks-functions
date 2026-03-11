c166ec73dde1752d6a1be7b8079e6248daf57084
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-11 21:32:58 +0100

    04 conta vocali with regex

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,16 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+const vocalSearch = (word) => {
+    reg= /a|e|i|o|u/g;
+    const vocalsFound = word.match(reg);
+    return vocalsFound.length + " " + vocalsFound;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+
+console.log(vocalSearch(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
